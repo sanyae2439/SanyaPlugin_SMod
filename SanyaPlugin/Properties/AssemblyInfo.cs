cf59927370a3661cd09f77740143d4fa84a0bee4
hatsunemiku24 <hatsunemiku_39@yahoo.co.jp>
2019-10-07 22:15:55 +0900

    13.6.6.3 assemblyInfo

--- SanyaPlugin/Properties/AssemblyInfo.cs
+++ SanyaPlugin/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("SanyaPlugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SCP:SL Smod Plugin")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("sanyae2439")]
 [assembly: AssemblyProduct("SanyaPlugin")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019 sanyae2439")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("13.6.6.3")]
+[assembly: AssemblyFileVersion("13.6.6.3")]
+[assembly: NeutralResourcesLanguage("ja-JP")]
